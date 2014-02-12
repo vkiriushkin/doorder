4c0eac2812531c589696b0a2f1f3ba22c331a5c0
Vladyslav Kiriushkin <vkiriushkin@gmail.com>
2014-02-12 23:10:20 +0200

    FXML changed. Pictures loaded

--- FxTest/src/main/java/sample/Controller.java
+++ FxTest/src/main/java/sample/Controller.java
@@ -5,6 +5,7 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 
 public class Controller {
@@ -12,9 +13,22 @@ public class Controller {
     public Text resultText;
     public TextField userName;
     public PasswordField passwordField;
+    public Pane doorTypesPane;
+    public ImageView andledDoorImageView;
+    public Pane orderPane;
 
     public void handleSubmitButtonAction(ActionEvent actionEvent) {
         resultText.setText(String.format("%s/%s",userName.getText(),passwordField.getText()));
 
     }
+
+    public void showOrderPane(ActionEvent actionEvent) {
+        orderPane.setVisible(true);
+    }
+
+    public void hideOrderPane(ActionEvent actionEvent) {
+        orderPane.setVisible(false);
+    }
+
+
 }
