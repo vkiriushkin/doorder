8bef3f90674ecb08f716e216c3f8939eeb80648a
Vladyslav Kiriushkin <vkiriushkin@gmail.com>
2014-02-16 23:32:52 +0200

    Keys types added

--- FxTest/src/main/java/sample/Controller.java
+++ FxTest/src/main/java/sample/Controller.java
@@ -6,6 +6,7 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 
@@ -14,28 +15,51 @@ import javax.swing.*;
 public class Controller {
 
     public Pane doorTypesPane;
-    public ImageView andledDoorImageView;
+    public AnchorPane doorsAnchorPane;
+    public Pane lockerTypesPane;
+    public AnchorPane lockerAnchorPane;
+    public Pane accessoriesPane;
     public Pane orderPane;
+
+    public Group apecs2000Group;
+    public Group kale2000Group;
     public Group metalDoorGroup;
     public Group fireProofGroup;
 
-    public void showOrderPane(ActionEvent actionEvent) {
-        doorTypesPane.setVisible(false);
-        orderPane.setVisible(true);
-    }
-
     public void showDoorTypesPane(ActionEvent actionEvent) {
         orderPane.setVisible(false);
+        lockerTypesPane.setVisible(false);
         doorTypesPane.setVisible(true);
     }
 
     public void showMetalDoorGroup(ActionEvent actionEvent) {
         fireProofGroup.setVisible(false);
         metalDoorGroup.setVisible(true);
+        doorsAnchorPane.setPrefHeight(metalDoorGroup.getLayoutBounds().getHeight());
     }
 
     public void showFireProofGroup(ActionEvent actionEvent) {
         metalDoorGroup.setVisible(false);
         fireProofGroup.setVisible(true);
+        doorsAnchorPane.setPrefHeight(fireProofGroup.getLayoutBounds().getHeight());
+        doorsAnchorPane.setMaxHeight(fireProofGroup.getLayoutBounds().getHeight());
+    }
+
+    public void showLockerTypesPane(ActionEvent actionEvent) {
+        orderPane.setVisible(false);
+        doorTypesPane.setVisible(false);
+        lockerTypesPane.setVisible(true);
+    }
+
+    public void showApecs2000Group(ActionEvent actionEvent) {
+        apecs2000Group.setVisible(true);
+        lockerAnchorPane.setPrefHeight(apecs2000Group.getLayoutBounds().getHeight());
+    }
+
+
+    public void showOrderPane(ActionEvent actionEvent) {
+        doorTypesPane.setVisible(false);
+        lockerTypesPane.setVisible(false);
+        orderPane.setVisible(true);
     }
 }
